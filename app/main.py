572a214205ce5b211adb56578a9e6c462e1e36b3
Kacper Lebocinski <kaclebocinski@gmail.com>
2025-03-31 12:03:16 +0200

    Fixed Solution

--- app/main.py
+++ app/main.py
@@ -9,16 +9,17 @@ def copy_file(command: str) -> None:
         first_file = str(formated_str[1])
         second_file = str(formated_str[2])
 
+        if len(formated_str) != 3 or formated_str[0] != "cp":
+            return
+
         if first_file == second_file:
             return
 
         if not os.path.isfile(first_file):
             return
 
-        if len(formated_str) != 3 or formated_str != "cp":
-            return
-
-        with open(second_file, "w") as for_write, open(first_file, "r") as for_read:
+        with (open(second_file, "w") as for_write,
+              open(first_file, "r") as for_read):
             for_write.write(for_read.read())
 
     except IndexError:
